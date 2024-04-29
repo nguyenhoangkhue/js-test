d5393297bb3580cc976fcbbdc1a321855398cce8
nguyenhoangkhue <snh110685@gmail.com>
2024-04-29 15:56:05 +0700

    upd

--- DOM/script.js
+++ DOM/script.js
@@ -19,3 +19,19 @@ const quizes = [
 window.addEventListener("contentLoaded",function(){
     
 })
+
+function randomAnswer(){
+    const questions=document.querySelectorAll(".quiz-item")
+    questions.forEach(function(question){
+        const options=question.querySelectorAll('input[type="radio"]')
+        const randomIndex=Math.floor(Math.random()*options.length)
+
+        options.forEach(function(option){
+            option.checked=false
+        })
+        options[randomIndex].checked=true
+    })
+}
+
+const btn=document.getElementById("btn")
+btn.addEventListener("click",randomAnswer)
